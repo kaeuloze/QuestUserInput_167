0c8e0b93e2a8995d104ee22c4e8f76543c8748c5
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-11-01 22:41:01 +0700

    Menambahkan state untuk input nama, alamat, jenis kelamin, dan status

--- app/src/main/java/com/example/userinput/Text2Edit.kt
+++ app/src/main/java/com/example/userinput/Text2Edit.kt
@@ -16,5 +16,8 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormBiodata(modifier: Modifier = Modifier) {
-
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
 }
