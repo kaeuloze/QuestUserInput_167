31d3e89b4e9eef617aa1423ceb0d96008f4c4431
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-11-01 22:38:20 +0700

    Mmebuat fungsi utama yang membentuk tampilan form biodata, dengan parameter opsional modifier

---
+++ app/src/main/java/com/example/userinput/Text2Edit.kt
@@ -0,0 +1,20 @@
+package com.example.userinput
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+
+
+@Composable
+fun FormBiodata(modifier: Modifier = Modifier) {
+
+}
